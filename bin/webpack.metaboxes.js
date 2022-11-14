e1120f9e769ea06b623d61f9797708ac8fe071ed
patrick leclech <pleclech@pleclech.com>
2022-11-14 23:52:33 +0100

    fixing fromEvent

--- bin/webpack.metaboxes.js
+++ bin/webpack.metaboxes.js
@@ -1,56 +1,61 @@
 /**
  * External dependencies.
  */
-const webpack = require( 'webpack' );
-const merge = require( 'webpack-merge' );
+const webpack = require("webpack");
+const merge = require("webpack-merge");
 
 /**
  * Internal dependencies.
  */
-const base = require( './webpack.base' );
-const paths = require( './paths' );
-const wpPackages = require( './wp-packages' );
+const base = require("./webpack.base");
+const paths = require("./paths");
+const wpPackages = require("./wp-packages");
 
 const config = {
 	entry: {
-		metaboxes: './packages/metaboxes/index.js'
+		metaboxes: "./packages/metaboxes/index.js",
 	},
 	output: {
-		library: [ 'cf', '[name]' ],
-		libraryTarget: 'this'
+		library: ["cf", "[name]"],
+		libraryTarget: "this",
 	},
 	externals: {
-		'react': [ 'cf', 'vendor', 'react' ],
-		'react-dom': [ 'cf', 'vendor', 'react-dom' ],
-		'nanoid': [ 'cf', 'vendor', 'nanoid' ],
-		'refract-callbag': [ 'cf', 'vendor', 'refract-callbag' ],
-		'callbag-basics': [ 'cf', 'vendor', 'callbag-basics' ],
-		'classnames': [ 'cf', 'vendor', 'classnames' ],
-		'immer': [ 'cf', 'vendor', 'immer' ],
-		'@carbon-fields/core': [ 'cf', 'core' ]
-	}
+		react: ["cf", "vendor", "react"],
+		"react-dom": ["cf", "vendor", "react-dom"],
+		nanoid: ["cf", "vendor", "nanoid"],
+		"refract-callbag": ["cf", "vendor", "refract-callbag"],
+		"callbag-basics": ["cf", "vendor", "callbag-basics"],
+		"callbag-from-delegated-event": [
+			"cf",
+			"vendor",
+			"callbag-from-delegated-event",
+		],
+		classnames: ["cf", "vendor", "classnames"],
+		immer: ["cf", "vendor", "immer"],
+		"@carbon-fields/core": ["cf", "core"],
+	},
 };
 
 module.exports = [
-	merge( base, config, {
+	merge(base, config, {
 		output: {
-			path: paths.gutenbergBuildPath
+			path: paths.gutenbergBuildPath,
 		},
-		externals: Object.assign( {}, wpPackages.externals, {
-			'lodash': [ 'lodash' ]
-		} )
-	} ),
-	merge( base, config, {
+		externals: Object.assign({}, wpPackages.externals, {
+			lodash: ["lodash"],
+		}),
+	}),
+	merge(base, config, {
 		output: {
-			path: paths.classicBuildPath
+			path: paths.classicBuildPath,
 		},
-		externals: Object.assign( {}, wpPackages.proxyExternals, {
-			'lodash': [ 'cf', 'vendor', 'lodash' ]
-		} ),
+		externals: Object.assign({}, wpPackages.proxyExternals, {
+			lodash: ["cf", "vendor", "lodash"],
+		}),
 		plugins: [
-			new webpack.ProvidePlugin( {
-				'wp.element': '@wordpress/element'
-			} )
-		]
-	} )
+			new webpack.ProvidePlugin({
+				"wp.element": "@wordpress/element",
+			}),
+		],
+	}),
 ];
