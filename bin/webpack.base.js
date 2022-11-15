4c8d0c15d5988dab97d8e28a811286cde320ee99
patrick leclech <pleclech@pleclech.com>
2022-11-15 04:00:38 +0100

    fixing callbag version

--- bin/webpack.base.js
+++ bin/webpack.base.js
@@ -1,21 +1,21 @@
 /**
  * External dependencies.
  */
-const path = require( 'path' );
-const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
-const OptimizeCssAssetsPlugin = require( 'optimize-css-assets-webpack-plugin' );
-const TerserPlugin = require( 'terser-webpack-plugin' );
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 /**
  * Indicates if we're running the build process in production mode.
  *
  * @type {Boolean}
  */
-const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
 	output: {
-		filename: isProduction ? '[name].min.js' : '[name].js'
+		filename: isProduction ? "[name].min.js" : "[name].js",
 	},
 	module: {
 		rules: [
@@ -23,63 +23,67 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
-					loader: 'babel-loader',
+					loader: "babel-loader",
 					options: {
-						cacheDirectory: true
-					}
-				}
+						cacheDirectory: true,
+					},
+				},
 			},
 			{
 				test: /\.scss$/,
 				use: [
 					MiniCssExtractPlugin.loader,
 					{
-						loader: 'css-loader',
+						loader: "css-loader",
 						options: {
-							importLoaders: 3
-						}
+							importLoaders: 3,
+						},
 					},
 					{
-						loader: 'postcss-loader'
+						loader: "postcss-loader",
 					},
 					{
-						loader: 'sass-loader'
+						loader: "sass-loader",
 					},
 					{
-						loader: 'sass-resources-loader',
+						loader: "sass-resources-loader",
 						options: {
-							resources: path.resolve( __dirname, '../assets/styles/*.scss' )
-						}
-					}
-				]
-			}
-		]
+							resources: path.resolve(
+								__dirname,
+								"../assets/styles/*.scss"
+							),
+						},
+					},
+				],
+			},
+		],
 	},
 	plugins: [
-		new MiniCssExtractPlugin( {
-			filename: isProduction ? '[name].min.css' : '[name].css'
-		} ),
+		new MiniCssExtractPlugin({
+			filename: isProduction ? "[name].min.css" : "[name].css",
+		}),
 
-		...(
-			isProduction
+		...(isProduction
 			? [
-				new OptimizeCssAssetsPlugin( {
-					cssProcessorPluginOptions: {
-						preset: [ 'default', { discardComments: { removeAll: true } } ]
-					}
-				} ),
-				new TerserPlugin( {
-					cache: true,
-					parallel: true
-				} )
-			]
-			: []
-		)
+					new OptimizeCssAssetsPlugin({
+						cssProcessorPluginOptions: {
+							preset: [
+								"default",
+								{ discardComments: { removeAll: true } },
+							],
+						},
+					}),
+					new TerserPlugin({
+						cache: true,
+						parallel: true,
+					}),
+			  ]
+			: []),
 	],
 	stats: {
 		modules: false,
 		hash: false,
 		builtAt: false,
-		children: false
-	}
+		children: false,
+	},
 };
