89d0b6d64195450f8a7bbb1af9631177c2827dda
patrick leclech <pleclech@pleclech.com>
2022-11-15 06:33:23 +0100

    fixing callbag version

--- packages/core/hocs/with-conditional-logic/index.js
+++ packages/core/hocs/with-conditional-logic/index.js
@@ -3,22 +3,16 @@
 /**
  * External dependencies.
  */
-import { createHigherOrderComponent, compose } from '@wordpress/compose';
-import { withDispatch, withSelect } from '@wordpress/data';
-import { withEffects } from 'refract-callbag';
-import { __, sprintf } from '@wordpress/i18n';
-import {
-	has,
-	find,
-	some,
-	every,
-	isEmpty
-} from 'lodash';
+import { createHigherOrderComponent, compose } from "@wordpress/compose";
+import { withDispatch, withSelect } from "@wordpress/data";
+import { withEffects } from "refract-callbag";
+import { __, sprintf } from "@wordpress/i18n";
+import { has, find, some, every, isEmpty } from "lodash";
 
 /**
  * Internal dependencies.
  */
-import compare from './compare';
+import compare from "./compare";
 
 /**
  * Creates a high-order components which adds ability to evalute
@@ -28,7 +22,7 @@ import compare from './compare';
  * @param  {Function} output
  * @return {Function}
  */
-export default function withConditionalLogic( input, output ) {
+export default function withConditionalLogic(input, output) {
 	/**
 	 * The function that controls the stream of side-effects.
 	 *
@@ -36,12 +30,12 @@ export default function withConditionalLogic( input, output ) {
 	 * @param  {Object} props
 	 * @return {Object}
 	 */
-	function aperture( component, props ) {
-		if ( isEmpty( props.field.conditional_logic ) ) {
-			return;
-		}
+	function aperture(component, props) {
+		//		if ( isEmpty( props.field.conditional_logic ) ) {
+		//			return;
+		//		}
 
-		return input( props, component );
+		return input(props, component);
 	}
 
 	/**
@@ -50,71 +44,84 @@ export default function withConditionalLogic( input, output ) {
 	 * @param  {Object} props
 	 * @return {Function}
 	 */
-	function handler( props ) {
-		return function( effect ) {
-			const fieldExists = has( effect, props.name ) || find( effect, [ 'id', props.id ] );
+	function handler(props) {
+		return function (effect) {
+			const fieldExists =
+				has(effect, props.name) || find(effect, ["id", props.id]);
 
-			if ( ! fieldExists ) {
+			if (!fieldExists) {
+				return;
+			}
+
+			if (isEmpty(props.field.conditional_logic)) {
+				props.showField(props.id);
 				return;
 			}
 
 			const { relation, rules } = props.field.conditional_logic;
-			const data = output( props, effect );
+			const data = output(props, effect);
 
-			const results = rules.reduce( ( accumulator, rule ) => {
-				if ( ! has( data, rule.field ) ) {
+			const results = rules.reduce((accumulator, rule) => {
+				if (!has(data, rule.field)) {
 					// eslint-disable-next-line
 					console.error(
 						sprintf(
-							__( 'An unknown field is used in condition - "%s"', 'carbon-fields-ui' ),
+							__(
+								'An unknown field is used in condition - "%s"',
+								"carbon-fields-ui"
+							),
 							rule.field
 						)
 					);
 
-					return accumulator.concat( false );
+					return accumulator.concat(false);
 				}
 
 				// TODO: Handle the conditional logic for chained fields. Probably we'll need the id of each sibling.
 				// See https://github.com/htmlburger/carbon-fields/commit/3628a86c8840c8323f45c829a96c512a9985ad10#diff-b1aea524a4b1ab510e28e01a54c25fcd
-				const result = compare( data[ rule.field ], rule.compare, rule.value );
+				const result = compare(
+					data[rule.field],
+					rule.compare,
+					rule.value
+				);
 
-				return accumulator.concat( result );
-			}, [] );
+				return accumulator.concat(result);
+			}, []);
 
 			let isVisible = false;
 
-			switch ( relation ) {
-				case 'AND':
-					isVisible = every( results );
+			switch (relation) {
+				case "AND":
+					isVisible = every(results);
 					break;
 
-				case 'OR':
-					isVisible = some( results );
+				case "OR":
+					isVisible = some(results);
 					break;
 			}
 
-			if ( isVisible ) {
-				props.showField( props.id );
+			if (isVisible) {
+				props.showField(props.id);
 			} else {
-				props.hideField( props.id );
+				props.hideField(props.id);
 			}
 		};
 	}
 
-	return createHigherOrderComponent( ( OriginalComponent ) => {
+	return createHigherOrderComponent((OriginalComponent) => {
 		return compose(
-			withDispatch( ( dispatch ) => {
-				const { showField, hideField } = dispatch( 'carbon-fields/core' );
+			withDispatch((dispatch) => {
+				const { showField, hideField } = dispatch("carbon-fields/core");
 
 				return {
 					showField,
-					hideField
+					hideField,
 				};
-			} ),
-			withSelect( ( select, props ) => ( {
-				visible: select( 'carbon-fields/core' ).isFieldVisible( props.id )
-			} ) ),
-			withEffects( aperture, { handler } )
-		)( OriginalComponent );
-	}, 'withConditionalLogic' );
+			}),
+			withSelect((select, props) => ({
+				visible: select("carbon-fields/core").isFieldVisible(props.id),
+			})),
+			withEffects(aperture, { handler })
+		)(OriginalComponent);
+	}, "withConditionalLogic");
 }
